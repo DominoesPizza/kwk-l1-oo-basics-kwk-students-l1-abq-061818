8f27307ea1ae75c9faa80347852127949919c0e2
DomIsNotOnFire <djpamcake38@gmail.com>
2018-06-22 19:49:01 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -74,3 +74,37 @@ def initialize(title)
   def page_count=(num)
     @page_count = num
   end
+  
+  def initialize(title)
+    @title = title
+  end
+ 
+  def title
+    @title
+  end
+ 
+  def author=(author)
+    @author = author
+  end
+ 
+  def author
+    @author
+  end
+ 
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+ 
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+ 
+end
