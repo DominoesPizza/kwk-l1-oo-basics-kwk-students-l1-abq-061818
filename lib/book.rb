913670b40c0b6960056d768b08be39eb4ecc048b
DomIsNotOnFire <djpamcake38@gmail.com>
2018-06-22 19:48:24 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -20,3 +20,19 @@ class Book
   end
  
 end
+
+class Book
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def title
+    @title
+  end
+ 
+  def author=(author)
+    @author = author
+  end
+ 
+end
